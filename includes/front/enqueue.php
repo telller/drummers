277d6b7fbbab1136e1155fa96fbb8acad9a57d2f
vipslord <my.yershov@gmail.com>
2017-06-29 14:34:20 +0300

    add bootstrap

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -2,11 +2,13 @@
   function hydraulics_enqueue() {
     wp_register_script( 'hydraulics_initial_store' , get_template_directory_uri() . '/hydraulics_app/dist/initialStore.js', array(), false, true );
     wp_register_script( 'hydraulics_app' , get_template_directory_uri() . '/hydraulics_app/dist/bundle.js', array(), false, true );
+    wp_register_style( 'hydraulics_bootstrap', get_template_directory_uri() . '/hydraulics_app/dist/bootstrap.css' );
     wp_register_style( 'hydraulics_styles', get_template_directory_uri() . '/hydraulics_app/dist/bundle.css' );
 
     wp_enqueue_script( 'hydraulics_initial_store' );
-    wp_enqueue_script( 'hydraulics_app' );
+    wp_enqueue_style( 'hydraulics_bootstrap' );
     wp_enqueue_style( 'hydraulics_styles' );
+    wp_enqueue_script( 'hydraulics_app' );
 
     wp_localize_script(
       'hydraulics_app',
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta charset=<?php bloginfo("charset"); ?>>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <title>Hydraulics</title>
     <?php wp_head() ?>
 </head>
