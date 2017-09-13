db9d041996d2fbc4ac28aacc2b60d2e0e727d1f8
EarlOld <dimonsuper010@gmail.com>
2017-09-13 21:02:18 +0300

    create connect

---
+++ hydraulics_app/app/connect.js
@@ -0,0 +1,10 @@
+import { editor } from './users'
+
+const WPAPI = require('wpapi')
+const WP = new WPAPI({
+  endpoint: `http://hydravlics.loc/wp-json`,
+  username: (editor().username),
+  password: (editor().password)
+})
+
+export default WP
